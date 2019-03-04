e9e524321fa8ad7e35c892f14cf731969a4d0706
Jamie Read <james.read@sideshowagency.com>
2019-03-04 12:27:43 +0000

    Fixed delete ingredient endpoint

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose'),
   User = mongoose.model('User');
 
 exports.getIngredients = function (req, res) {
-  User.find({ _id: req.params.id }, (err, user) => {
+  User.findOne({ _id: req.params.id }, (err, user) => {
     if (err)
       res.send(err);
     res.json(user.ingredients);
@@ -17,7 +17,6 @@ exports.addIngredient = function (req, res) {
     (err, ingredient) => {
       if (err)
         res.send(err);
-      console.log(ingredient)
       res.json({ message: 'Ingredient successfully added' });
   });
 };
@@ -25,7 +24,7 @@ exports.addIngredient = function (req, res) {
 exports.deleteIngredient = function (req, res) {
   User.update(
     { _id: req.params.id }, 
-    { $pull: { _id: { _id: req.params.ingredientId } } },
+    { $pull: { ingredients: { _id: req.params.ingredientId } } },
     (err, ingredient) => {
       if (err)
         res.send(err);
